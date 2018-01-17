72ddd4d29c3134f5f362f6b6d0b97c3becd5f32c
Julia <julsuniverse@gmail.com>
2018-01-17 14:59:02 +0200

    init2

---
+++ src/Framework/Http/Request.php
@@ -0,0 +1,14 @@
+<?php
+namespace Framework\Http;
+
+class Request
+{
+    public function getQueryParams(): array
+    {
+        return $_GET;
+    }
+    public function getParsedBody()
+    {
+        return $_POST ?: null;
+    }
+}
