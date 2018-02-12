a67b7f82f02539da6570b97218d581d08ba6d988
Julia <julsuniverse@gmail.com>
2018-02-12 17:58:54 +0200

    refactored config

--- config/config.php
+++ config/config.php
@@ -1,5 +1,7 @@
 <?php
 return array_merge_recursive(
-    require __DIR__ . '/dependencies.php',
-    require __DIR__ . '/parameters.php'
-);
+    require __DIR__ . '/autoload/app.global.php',
+    require __DIR__ . '/autoload/auth.global.php',
+    require __DIR__ . '/autoload/app.local.php',
+    require __DIR__ . '/autoload/auth.local.php',
+    require __DIR__ . '/autoload/local.php');
